d302d5ad8a927c529bc2e6b584a034048775b50f
Nagarjun H M <arjunahms123@gmail.com>
2023-11-15 12:34:20 +0530

    Update ErrorElement
    added home button to navigate back

--- src/pages/ErrorElement.jsx
+++ src/pages/ErrorElement.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { TbError404 } from "react-icons/tb";
+import { useNavigate } from 'react-router-dom';
 
 const ErrorElement = () => {
+    const navigate = useNavigate();
   return (
-    <div className='flex items-center justify-center h-screen text-4xl font-semibold tracking-wider text-red-500'>
-    <TbError404 size="10em"/>
+    <div className='flex justify-center'>
+    <div className='flex items-center justify-center h-screen mx-5 text-4xl font-semibold tracking-wider text-red-500'>
+    <div className='flex-grow'>
+        <button className="btn " onClick={()=>{
+            navigate("/");
+        }}>
+            Home
+        </button>
+</div>
+    <div className=''><TbError404 size="10em" /></div>
     <div className='p-3 border-b-2 border-red-500'>Something went Wrong</div>
+    </div>
     </div>
   )
 }
